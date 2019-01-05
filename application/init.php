86ade10cb93265ad811e00d25971cb389d3fd576
arjunkashyap <arjun.kashyap@yahoo.co.in>
2019-01-05 13:42:49 +0530

    no libraries used

--- application/init.php
+++ application/init.php
@@ -7,8 +7,8 @@ require_once 'core/Controller.php';
 require_once 'core/Database.php';
 
 // Vendor calls
-require_once 'vendor/autoload.php';
-require_once 'vendor/Git.php';
+// require_once 'vendor/autoload.php';
+// require_once 'vendor/Git.php';
 
 require_once 'config/config.php';
 require_once 'config/settings.php';
